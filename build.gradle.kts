39325e8dce70d23b0c4cfd2ab314a3ac78309d4d
Martin Vysny <mavi@vaadin.com>
2021-08-30 19:13:48 +0300

    initial import

---
+++ build.gradle.kts
@@ -0,0 +1,92 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.30"
+    `maven-publish`
+    signing
+}
+
+defaultTasks("clean", "build")
+
+group = "com.gitub.mvysny.karibu-tools"
+version = "0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    explicitApi()
+}
+
+dependencies {
+    api(kotlin("stdlib-jdk8")) // don't use -jdk8 to stay compatible with Android
+    testImplementation("com.github.mvysny.dynatest:dynatest-engine:0.20")
+}
+
+// following https://dev.to/kengotoda/deploying-to-ossrh-with-gradle-in-2020-1lhi
+java {
+    withJavadocJar()
+    withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<Javadoc> {
+    isFailOnError = false
+}
+
+publishing {
+    repositories {
+        maven {
+            setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = project.properties["ossrhUsername"] as String? ?: "Unknown user"
+                password = project.properties["ossrhPassword"] as String? ?: "Unknown user"
+            }
+        }
+    }
+    publications {
+        create("mavenJava", MavenPublication::class.java).apply {
+            groupId = project.group.toString()
+            this.artifactId = "karibu-tools"
+            version = project.version.toString()
+            pom {
+                description.set("Karibu-Tools: The Vaadin Missing Helpers")
+                name.set("Karibu-Tools")
+                url.set("https://github.com/mvysny/karibu-tools")
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("mavi")
+                        name.set("Martin Vysny")
+                        email.set("martin@vysny.me")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/mvysny/karibu-tools")
+                }
+            }
+            from(components["java"])
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        // to see the exceptions of failed tests in Travis-CI console.
+        exceptionFormat = TestExceptionFormat.FULL
+    }
+}
