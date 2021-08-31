bf31370f2629c928e4e177b25cad849ed0f8f1e0
Martin Vysny <mavi@vaadin.com>
2021-08-31 20:14:26 +0300

    Implement TextFieldUtils

---
+++ src/main/kotlin/TextFieldUtils.kt
@@ -0,0 +1,78 @@
+package com.github.mvysny.kaributools
+
+import com.vaadin.flow.component.textfield.GeneratedVaadinTextArea
+import com.vaadin.flow.component.textfield.GeneratedVaadinTextField
+
+/**
+ * Selects all text in this text field. The selection is not really visible in
+ * the browser unless the field is focused.
+ */
+public fun GeneratedVaadinTextField<*, String>.selectAll() {
+    element.executeJs("this.inputElement.select()");
+}
+
+/**
+ * Clears the selection in the text field and moves the cursor to the end of
+ * the text. There may be no effect if the field is unfocused - the browser
+ * generally selects all when the field gains focus.
+ */
+public fun <R: GeneratedVaadinTextField<R, String>> R.selectNone() {
+    setCursorLocation(value?.length ?: 0)
+}
+
+/**
+ * Moves the cursor within the text field. Has the side-effect of clearing the selection.
+ *
+ * There may be no effect if the field is unfocused - the browser
+ * generally selects all when the field gains focus.
+ */
+public fun GeneratedVaadinTextField<*, String>.setCursorLocation(cursorLocation: Int) {
+    select(cursorLocation until cursorLocation)
+}
+
+/**
+ * Selects given characters in this text field. The selection is not really visible in
+ * the browser unless the field is focused.
+ */
+public fun GeneratedVaadinTextField<*, String>.select(selection: IntRange) {
+    require(selection.first >= 0)
+    require(selection.last >= -1)
+    element.executeJs("this.inputElement.setSelectionRange(${selection.first}, ${selection.last + 1})")
+}
+
+/**
+ * Selects all text in this text field. The selection is not really visible in
+ * the browser unless the field is focused.
+ */
+public fun GeneratedVaadinTextArea<*, String>.selectAll() {
+    element.executeJs("this.inputElement.select()")
+}
+
+/**
+ * Clears the selection in the text field and moves the cursor to the end of
+ * the text. There may be no effect if the field is unfocused - the browser
+ * generally selects all when the field gains focus.
+ */
+public fun <R: GeneratedVaadinTextArea<R, String>> R.selectNone() {
+    setCursorLocation(value?.length ?: 0)
+}
+
+/**
+ * Moves the cursor within the text field. Has the side-effect of clearing the selection.
+ *
+ * There may be no effect if the field is unfocused - the browser
+ * generally selects all when the field gains focus.
+ */
+public fun GeneratedVaadinTextArea<*, String>.setCursorLocation(cursorLocation: Int) {
+    select(cursorLocation until cursorLocation)
+}
+
+/**
+ * Selects given characters in this text field. The selection is not really visible in
+ * the browser unless the field is focused.
+ */
+public fun GeneratedVaadinTextArea<*, String>.select(selection: IntRange) {
+    require(selection.first >= 0)
+    require(selection.last >= -1)
+    element.executeJs("this.inputElement.setSelectionRange(${selection.first}, ${selection.last + 1})")
+}
---
+++ src/test/kotlin/TextFieldUtilsTest.kt
@@ -0,0 +1,36 @@
+package com.github.mvysny.kaributools
+
+import com.github.mvysny.dynatest.DynaTest
+import com.github.mvysny.kaributesting.v10.MockVaadin
+import com.vaadin.flow.component.textfield.EmailField
+import com.vaadin.flow.component.textfield.TextArea
+import com.vaadin.flow.component.textfield.TextField
+
+class TextFieldUtilsTest : DynaTest({
+    beforeEach { MockVaadin.setup() }
+    afterEach { MockVaadin.tearDown() }
+
+    group("TextField") {
+        test("smoke") {
+            TextField().selectAll()
+            TextField().selectNone()
+            TextField().setCursorLocation(3)
+        }
+    }
+
+    group("TextArea") {
+        test("smoke") {
+            TextArea().selectAll()
+            TextArea().selectNone()
+            TextArea().setCursorLocation(3)
+        }
+    }
+
+    group("EmailField") {
+        test("smoke") {
+            EmailField().selectAll()
+            EmailField().selectNone()
+            EmailField().setCursorLocation(3)
+        }
+    }
+})
