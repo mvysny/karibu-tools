52fdeefce701322e80ff7ea1a84109f65a0b5586
Martin Vysny <mavi@vaadin.com>
2021-11-04 12:24:33 +0200

    0.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.karibu-tools"
-    version = "0.6-SNAPSHOT"
+    version = "0.6"
 
     apply {
         plugin("kotlin")
