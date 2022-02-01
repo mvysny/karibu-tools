352e5086b85222012703d92c875deab58e35717b
Martin Vysny <mavi@vaadin.com>
2022-02-01 11:14:40 +0200

    Vaadin 23: only test on jdk11

--- testsuite/testrun-vaadin23/src/test/kotlin/AllTests.kt
+++ testsuite/testrun-vaadin23/src/test/kotlin/AllTests.kt
@@ -12,19 +12,19 @@ import java.util.*
 import kotlin.test.expect
 
 class AllTests : DynaTest({
-    test("vaadin version") {
-        expect(23) { VaadinVersion.get.major }
-        VaadinVersion.flow // smoke test that the call doesn't fail
-    }
-
-    test("vaadin version 2") {
-        val gradleProps: Properties = File("../../gradle.properties").loadAsProperties()
-        val expectedVaadinVersion: String = gradleProps["vaadin23_version"] as String
-        expect(expectedVaadinVersion) { VaadinVersion.get.toString().replace('-', '.') }
-    }
-
     // Vaadin 23+ requires JDK 11+
     if (jvmVersion >= 11) {
+        test("vaadin version") {
+            expect(23) { VaadinVersion.get.major }
+            VaadinVersion.flow // smoke test that the call doesn't fail
+        }
+
+        test("vaadin version 2") {
+            val gradleProps: Properties = File("../../gradle.properties").loadAsProperties()
+            val expectedVaadinVersion: String = gradleProps["vaadin23_version"] as String
+            expect(expectedVaadinVersion) { VaadinVersion.get.toString().replace('-', '.') }
+        }
+
         group("vaadin14") {
             allTests()
         }
