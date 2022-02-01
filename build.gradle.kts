1bd24e0f9af699db73e428aaee9337c6c72fef82
Martin Vysny <mavi@vaadin.com>
2022-02-01 11:52:11 +0200

    kotlin: use 1.8 jvm target

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,10 @@ allprojects {
             exceptionFormat = TestExceptionFormat.FULL
         }
     }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
 }
 
 kotlin {
