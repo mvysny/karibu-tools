93249f109b3c8dfe7e6b6e9f90287fcf09d19285
Martin Vysny <mavi@vaadin.com>
2022-03-29 08:35:02 +0300

    remove tests for vaadin 21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ include(
     "testsuite:vaadin21",
     "testsuite:testrun-vaadin14-stable",
     "testsuite:testrun-vaadin14-next",
-    "testsuite:testrun-vaadin21",
     "testsuite:testrun-vaadin22",
     "testsuite:testrun-vaadin23",
 )
--- testsuite/testrun-vaadin21/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-dependencies {
-    testImplementation("com.vaadin:vaadin-core:${properties["vaadin21_version"]}")
-    testImplementation(project(":testsuite:vaadin14")) {
-        exclude(group = "com.vaadin")
-    }
-    testImplementation(project(":testsuite:vaadin21")) {
-        exclude(group = "com.vaadin")
-    }
-}
