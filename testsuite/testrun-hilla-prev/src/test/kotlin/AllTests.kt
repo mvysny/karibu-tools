8b46c5851ffe935822b032dcae87eb389bd5f2fa
Martin Vysny <mavi@vaadin.com>
2023-06-13 14:13:11 +0300

    tests

--- testsuite/testrun-hilla-prev/src/test/kotlin/AllTests.kt
+++ testsuite/testrun-hilla-prev/src/test/kotlin/AllTests.kt
@@ -2,6 +2,7 @@ package com.github.mvysny.kaributools.v22
 
 import com.github.mvysny.dynatest.DynaTest
 import com.github.mvysny.kaributools.*
+import com.vaadin.flow.server.Platform
 import loadAsProperties
 import java.io.File
 import java.util.*
@@ -12,5 +13,8 @@ class AllTests : DynaTest({
         val gradleProps: Properties = File("../../gradle.properties").loadAsProperties()
         val expectedHillaVersion: String = gradleProps["hilla_prev_version"] as String
         expect(expectedHillaVersion) { VaadinVersion.hilla.toString() }
+        // Platform.getVaadinVersion() returns Hilla version if Hilla is on the classpath. Implementation detail?
+        // See https://github.com/vaadin/hilla/issues/1022
+        expect(Platform.getVaadinVersion().orElse(null)) { VaadinVersion.hilla.toString() }
     }
 })
