2b5bc175e9e1bc3a11afb56245811acfc7b8d706
Martin Vysny <mavi@vaadin.com>
2024-01-15 10:59:59 +0200

    0.19-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.karibu-tools"
-    version = "0.18"
+    version = "0.19-SNAPSHOT"
 
     repositories {
         mavenCentral()
