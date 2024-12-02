00ec02f89a0b7a051cbf3f1a6f0ca000565a63e9
Martin Vysny <mavi@vaadin.com>
2024-12-02 09:08:14 +0200

    replace dynatest with junit5

--- testsuite/testrun-vaadin24prev/src/test/kotlin/AllTests.kt
+++ testsuite/testrun-vaadin24prev/src/test/kotlin/AllTests.kt
@@ -1,37 +1,34 @@
 package com.github.mvysny.kaributools.v24prev
 
-import allTests21
-import allTests23
-import com.github.mvysny.dynatest.DynaTest
+import AbstractAllTests21
+import AbstractAllTests23
 import com.github.mvysny.kaributools.*
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import org.tomlj.TomlTable
 import java.io.File
-import java.util.*
 import kotlin.test.expect
 
-class AllTests : DynaTest({
-    test("vaadin version") {
+class AllTests {
+    @Test fun `vaadin version`() {
         expect(24) { VaadinVersion.get.major }
         VaadinVersion.flow // smoke test that the call doesn't fail
     }
 
-    test("hilla version") {
+    @Test fun `hilla version`() {
         expect(null) { VaadinVersion.hilla }
     }
 
-    test("vaadin version 2") {
+    @Test fun `vaadin version 2`() {
         val gradleProps: TomlTable = File("../../gradle/libs.versions.toml").parseToml()
         val expectedVaadinVersion: String = gradleProps["versions.vaadin24"] as String
         expect(expectedVaadinVersion) { VaadinVersion.get.toString().replace('-', '.') }
     }
 
-    group("vaadin14") {
-        allTests()
-    }
-    group("vaadin21+") {
-        allTests21()
-    }
-    group("vaadin23+") {
-        allTests23()
-    }
-})
+    @Nested
+    inner class AllTests : AbstractAllTests()
+    @Nested
+    inner class AllTests21 : AbstractAllTests21()
+    @Nested
+    inner class AllTests23 : AbstractAllTests23()
+}
