5529bed74624db5150bb59a8be417cf6cd2a0a56
Vitalii Lipovetskii <vlipovetskii@users.noreply.github.com>
2025-03-24 17:47:16 +0200

    MimeType naming improvements

--- karibu-tools/src/main/kotlin/MimeType.kt
+++ karibu-tools/src/main/kotlin/MimeType.kt
@@ -34,20 +34,20 @@ public data class MimeType(val mimeContentType: ContentType, val contentSubType:
     public companion object {
 
         public val TEXT_PLAIN: MimeType = MimeType(ContentType.TEXT, "plain")
-        public val HTML: MimeType = MimeType(ContentType.TEXT, "html")
+        public val TEXT_HTML: MimeType = MimeType(ContentType.TEXT, "html")
         public val XLS: MimeType = MimeType(ContentType.APPLICATION, "vnd.ms-excel")
         public val XLSX: MimeType = MimeType(ContentType.APPLICATION, "vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 
         public val IMAGE: MimeType = MimeType(ContentType.IMAGE)
-        public val JPEG: MimeType = MimeType(ContentType.IMAGE, "jpeg")
+        public val IMAGE_JPEG: MimeType = MimeType(ContentType.IMAGE, "jpeg")
 
         public val AUDIO: MimeType = MimeType(ContentType.AUDIO)
-        public val MP3: MimeType = MimeType(ContentType.AUDIO, "mpeg")
+        public val AUDIO_MP3: MimeType = MimeType(ContentType.AUDIO, "mpeg")
 
         public val VIDEO: MimeType = MimeType(ContentType.VIDEO)
-        public val AVI: MimeType = MimeType(ContentType.VIDEO, "x-msvideo")
-        public val MPEG: MimeType = MimeType(ContentType.VIDEO, "mpeg")
-        public val MP4: MimeType = MimeType(ContentType.VIDEO, "mp4")
+        public val VIDEO_AVI: MimeType = MimeType(ContentType.VIDEO, "x-msvideo")
+        public val VIDEO_MPEG: MimeType = MimeType(ContentType.VIDEO, "mpeg")
+        public val VIDEO_MP4: MimeType = MimeType(ContentType.VIDEO, "mp4")
 
     }
 
