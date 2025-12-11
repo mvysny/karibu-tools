fe5714ff21b024e3ad3ab2ceea3a05864c555d8f
Martin Vysny <mavi@vaadin.com>
2025-12-11 10:57:06 +0200

    upgrade Maven release procedure

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -11,6 +11,6 @@ To release the library to Maven Central:
 3. git tag the commit with the same tag name as the commit message above, e.g. `0.20`
 4. `git push`, `git push --tags`
 5. Run `./gradlew clean build publish`
-6. Continue to the [OSSRH Nexus](https://oss.sonatype.org/#stagingRepositories) and follow the [release procedure](https://central.sonatype.org/pages/releasing-the-deployment.html).
+6. (Optional) watch [Maven Central Publishing Deployments](https://central.sonatype.com/publishing/deployments) as the deployment is published.
 7. Add the `-SNAPSHOT` back to the `version=` while increasing the version to something which will be released in the future,
    e.g. 0.21-SNAPSHOT, then commit with the commit message "0.21-SNAPSHOT" and push.
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "2.2.20"
     `maven-publish`
     signing
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 
 defaultTasks("clean", "build")
@@ -57,15 +58,6 @@ subprojects {
         }
 
         publishing {
-            repositories {
-                maven {
-                    setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-                    credentials {
-                        username = project.properties["ossrhUsername"] as String? ?: "Unknown user"
-                        password = project.properties["ossrhPassword"] as String? ?: "Unknown user"
-                    }
-                }
-            }
             publications {
                 create("mavenJava", MavenPublication::class.java).apply {
                     groupId = project.group.toString()
@@ -100,6 +92,16 @@ subprojects {
 
         signing {
             sign(publishing.publications["mavenJava"])
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        // see https://central.sonatype.org/publish/publish-portal-ossrh-staging-api/#configuration
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
         }
     }
 }
